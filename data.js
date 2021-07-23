d91a2f4c22ff8ca743cd82e6804e2394e03706dd
cgw93 <87839059+cgw93@users.noreply.github.com>
2021-07-23 10:24:57 +0900

    Add files via upload

---
+++ data.js
@@ -0,0 +1,68 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-mbpbcrtwt7lykknwc62lcepqdjd_ebln",
+      "name": "mbpbcRtwt7LykKNWC62lCEpQDJd_ebLn",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.004598791898223098,
+        "pitch": 0,
+        "fov": 1.2848183641643347
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.21902456540450999,
+          "pitch": 0.0851679383935462,
+          "rotation": 6.283185307179586,
+          "target": "1-stiches_comped_proxy_01"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-stiches_comped_proxy_01",
+      "name": "stiches_comped_proxy_01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 192,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
